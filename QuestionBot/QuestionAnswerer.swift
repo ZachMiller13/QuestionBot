83a8e6a08dc9598a609017e52b0068612f0dea2f
Zachary Miller <zach_miller13@icloud.com>
2022-02-24 21:00:22 -0500

    Completion of playground activity, copy/paste to project file.

--- QuestionBot/QuestionAnswerer.swift
+++ QuestionBot/QuestionAnswerer.swift
@@ -1,6 +1,21 @@
 struct MyQuestionAnswerer {
     func responseTo(question: String) -> String {
-        // TODO: Write a response
-        return "I'm sorry, I don't understand the question."
+        let lowerQuestion = question.lowercased()
+        
+        if lowerQuestion.hasPrefix("hello") {
+            return "Why, hello there!"
+        } else if lowerQuestion == "where are the cookies?" {
+            return "In the cookie jar!"
+        } else if lowerQuestion.hasPrefix("where") {
+            return "To the North!"
+        } else if lowerQuestion.hasPrefix("can") {
+            return "I don't know, can you?"
+        } else if lowerQuestion.hasPrefix("should") {
+            return "You should do whatever your little heart desires."
+        } else {
+            return "Stop asking me questions!"
+        }
+        
+        
     }
 }
